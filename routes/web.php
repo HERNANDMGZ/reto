41e5f41246465ba8dc767474a14f1963378b0cf3
HERNANDMGZ <HERNANDMGZ1987@gmail.com>
2020-07-10 16:42:40 -0500

    creacion columna is_active

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
 Route::resource ('usuarios', 'UserController')->middleware('auth');
 
