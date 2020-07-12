49bc0887a1d0a4375f17da8c9016c398abca76dc
HERNANDMGZ <HERNANDMGZ1987@gmail.com>
2020-07-12 11:16:28 -0500

    products/index.blade.php Structure Design

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,33 @@
+@extends ('layout.app')
+
+@section('content')
+
+<div class="big-padding text-center blue-grey white-text">
+    <h1>PRODUCTOS </h1>
+</div>
+<div class="container">
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <td>Id</td>
+            <td>Titulo</td>
+            <td>Descripcion</td>
+            <td>Precio</td>
+            <td>Herramientas</td>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</div>
+
+
+
+
+
+
+
+@endsection
+
+
