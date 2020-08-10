03d86e0ab5ac868d0c4ecc004889877b249c232d
HERNANDMGZ <HERNANDMGZ1987@gmail.com>
2020-08-09 22:33:09 -0500

    feat: show products in welcome view, index method in HomeController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,11 +13,27 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    MERCATODO
+                <div class="grid-container">
+                    @forelse($products as $product)
+                        <div class="grid-item">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="/storage/{{$product->image}}" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{$product->name}}</h5>
+                                    <p class="card-text">{{ $product ->description }}</p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">col ${{$product->pricing}}</li>
+                                </ul>
+                            </div>
+                        </div>
                 </div>
-            </div>
+                        @empty
+                            <p>No existen productos que mostrar</p>
+                        @endforelse
+                </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
