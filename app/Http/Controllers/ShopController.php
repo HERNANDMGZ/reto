361d7f32190da4364327f7c3eefdf406d9570785
HERNANDMGZ <HERNANDMGZ1987@gmail.com>
2020-10-22 22:08:05 -0500

    feat: add methods of Article Shops in ShopController

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 
 use App\Category;
+use App\Order;
 use App\Product;
+use App\ProductOrder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class ShopController extends Controller
@@ -91,5 +94,68 @@ class ShopController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function addToCart(Product $product, Request $request)
+    {
+
+        $order = $this->generateOrder();
+
+        $productUser = new ProductOrder();
+        $productUser->product_id = $product->id;
+        $productUser->order_id = $order->id;
+        $productUser->product_pricing = $product->pricing * $request['quantity'];
+        $productUser->quantity = $request['quantity'];
+
+        $productUser->save();
+
+        $totalPricing = ProductOrder::sum('product_pricing');
+        $this->updateTotalPrice($totalPricing, $order->id);
+
+        return redirect('shops');
+
+    }
+
+    private function generateOrder()
+    {
+
+        $user = Auth::user();
+
+        $orderGenerate = Order::where('user_id', $user->id)
+            ->where('status', 'cotizacion')
+            ->first();
+
+        if(!empty($orderGenerate))
+        {
+            return $orderGenerate;
+        }
+        $order = new Order();
+        $order->user_id = $user->id;
+        $order->status = 'cotizacion';
+
+        $order->save();
+
+        return $order;
+    }
+
+    public function showCart()
+    {
+        $user = Auth::user();
+
+        $orderGenerate = Order::where('user_id', $user->id)
+            ->where('status', 'cotizacion')
+            ->first();
+
+        $products = ProductOrder::where('order_id', $orderGenerate->id)->get();
+
+        return view('shops.showCart', ['products' => $products, 'totalPricing' => $orderGenerate->total_price]);
+    }
+
+    private function updateTotalPrice($totalPricing, $id)
+    {
+        $order = Order::findOrFail($id);
+        $order->total_price  = $totalPricing;
+
+        $order->update();
     }
 }
