80e339b3caad3d28c754317771d8df4828d26adc
HERNANDMGZ <HERNANDMGZ1987@gmail.com>
2020-10-24 18:36:33 -0500

    feat: new Shopping Cart View and Checkout View

--- resources/views/shops/showCart.blade.php
+++ resources/views/shops/showCart.blade.php
@@ -1,34 +1,64 @@
 @extends ('layouts.app')
 
 @section('content')
-    <table class="table">
-        <thead>
-        <tr>
-            <th>item</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>total Price</th>
-            <th>Action</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($products as $product)
-            <tr>
-                <td>{{$product->id}}</td>
-                <td>{{$product->find($product->id)->name}}</td>
-                <td>{{$product->find($product->id)->pricing}}</td>
-                <td>{{$product->quantity}}</td>
-                <td>{{$product->product_pricing}}</td>
-                <td></td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-    <h1>total de factura: {{ $totalPricing }}</h1>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
 
-    <a>pagar</a>
-
-
-
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <div class="panel-title">
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <h5><span class="glyphicon glyphicon-shopping-cart"></span> Carrito de Compras</h5>
+                                </div>
+                                <div class="col-xs-4">
+                                    <a href="/shops" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Continuar Comprando</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        @foreach($products as $product)
+                        <div class="row">
+                            <div class="col-xs-2"><img class="img-responsive" src="http://placehold.it/100x70">
+                            </div>
+                            <div class="col-xs-4">
+                                <h4 class="product-name"><strong>{{$product->find($product->id)->name}}</strong></h4><h4><small>Product description</small></h4>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="col-xs-6 text-right">
+                                    <h6><strong>$ {{$product->find($product->id)->pricing}}<span class="text-muted"> X </span></strong></h6>
+                                </div>
+                                <div class="col-xs-4">
+                                    <h6 class="product-name"><strong>{{$product->quantity}}</strong>Unidades</h6>
+                                </div>
+                                <div class="col-xs-2">
+                                    <button type="button" class="btn btn-link btn-xs">
+                                        <span class="glyphicon glyphicon-trash"> </span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                        <hr>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="row text-center">
+                            <div class="col-xs-9">
+                                <h4 class="text-right">Total <strong>{{ $totalPricing }}</strong></h4>
+                            </div>
+                            <div class="col-xs-3">
+                                <a class="btn btn-primary" href="{{route('shops.getCheckout')}}" role="button">Resumen</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
