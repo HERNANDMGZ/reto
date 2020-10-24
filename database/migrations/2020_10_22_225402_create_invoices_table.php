4fbb8d38eec7769099dff96c73edbce3b0862451
HERNANDMGZ <HERNANDMGZ1987@gmail.com>
2020-10-24 18:38:06 -0500

    feat: invoice migration, add new field called name - with model

---
+++ database/migrations/2020_10_22_225402_create_invoices_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->string('name');
+            $table->string('status');
+            $table->string('total_price');
+            $table->string('address_payment');
+            $table->string('reference');
+            $table->dateTime('expiration');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
