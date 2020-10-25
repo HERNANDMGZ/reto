dc28cb7f88d1e0054c4899443662be6025dba958
HERNANDMGZ <HERNANDMGZ1987@gmail.com>
2020-10-24 20:50:33 -0500

    refactor: update files and package

--- public/js/app.js
+++ public/js/app.js
@@ -37360,8 +37360,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\reto\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\reto\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\evertec\reto\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\evertec\reto\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
